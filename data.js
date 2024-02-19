28634c5a618e4519a1feb4a4db8bdfe3904c18a7
Galih Nugraha <galihnugraha58@gmail.com>
2024-02-19 18:01:13 +0700

    bug pada charts

--- data.js
+++ data.js
@@ -68,3 +68,5 @@ const dataUser = {
       }
     ]
   }
+
+export default dataUser
--- script.js
+++ script.js
@@ -1,87 +1,42 @@
-for (let i = 0; i < dataUser.length; i++) {
-    movies.push(dataUser[i])
+import dataUser from "./data.js"
+
+const showContainer = (dataUser) => {
+  return `<div class="row">
+              <div class="col-md-4">
+                  <p>Name : ${dataUser.name}</p>
+                  <p>Phone : ${dataUser.phone}</p>
+                  <p>Address : ${dataUser.address}</p>
+              </div>
+              <div class="col-md-4">
+                  <p>Age : ${dataUser.age}</p>
+                  <p>Education : ${dataUser.education}</p>
+                  <p>Mail : ${dataUser.mail}</p>
+              </div>
+          </div>`
 }
 
-const showCards = (dataUser) => {
-    return `<div class="col-md-4 my-3">
-                <div class="card" style="min-height: 690px">
-                    <img class="card-img-top" src="${dataUser.Poster}" alt="poster film ${dataUser.Title}">
-                    <div class="card-body">
-                        <h5 class="card-title">${dataUser.Title}</h5>
-                        <h6 class="card-subtitle mb-2 text-muted">${dataUser.Year}</h6>
-                        <a href="#" data-toggle="modal" data-target="#movieDetailModal" class="btn btn-primary modal-detail-button" data-imdbid="${movie.imdbID}">Show Details</a>
-                    </div>
-                </div>
-            </div>`
+const showCards = (dataUser, i) => {
+  return `<div class="col-md-4 my-3">
+              <div class="card" style="min-height: 500px">
+                  <div class="card-body">
+                  <img class="card-img-top" src="${dataUser.charts[i].thumbnail}" alt="poster film ${dataUser.Title}">
+                      <h5 class="card-title">${dataUser.charts[i].title}</h5>
+                      <h6 class="card-subtitle mb-2 text-muted">Price : $${dataUser.charts[i].price}</h6>
+                      <h6 class="card-subtitle mb-2 text-muted">Discount : $${dataUser.charts[i].discountPercentage}</h6>
+                      <br>
+                      <h6 class="card-subtitle mb-2 text-muted">${dataUser.charts[i].description}</h6>
+                  </div>
+              </div>
+          </div>`
 }
 
-const dataUser = {
-    "name": "Samantha Lee",
-    "address": "321 Pine St, Anytown, USA",
-    "phone": "(555) 555-5555",
-    "age": 35,
-    "email": "samanthalee@example.com",
-    "education": "Doctorate in Psychology",
-    "charts": [
-      {
-        "id": 1,
-        "title": "iPhone 9",
-        "description": "An apple mobile which is nothing like apple",
-        "price": 549,
-        "discountPercentage": 12.96,
-        "rating": 4.69,
-        "stock": 94,
-        "brand": "Apple",
-        "category": "smartphones",
-        "thumbnail": "https://i.dummyjson.com/data/products/1/thumbnail.jpg"
-      },
-      {
-        "id": 2,
-        "title": "iPhone X",
-        "description": "SIM-Free, Model A19211 6.5-inch Super Retina HD display with OLED technology A12 Bionic chip with ...",
-        "price": 899,
-        "discountPercentage": 17.94,
-        "rating": 4.44,
-        "stock": 34,
-        "brand": "Apple",
-        "category": "smartphones",
-        "thumbnail": "https://i.dummyjson.com/data/products/2/thumbnail.jpg"
-      },
-      {
-        "id": 3,
-        "title": "Samsung Universe 9",
-        "description": "Samsung's new variant which goes beyond Galaxy to the Universe",
-        "price": 1249,
-        "discountPercentage": 15.46,
-        "rating": 4.09,
-        "stock": 36,
-        "brand": "Samsung",
-        "category": "smartphones",
-        "thumbnail": "https://i.dummyjson.com/data/products/3/thumbnail.jpg"
-      },
-      {
-        "id": 4,
-        "title": "OPPOF19",
-        "description": "OPPO F19 is officially announced on April 2021.",
-        "price": 280,
-        "discountPercentage": 17.91,
-        "rating": 4.3,
-        "stock": 123,
-        "brand": "OPPO",
-        "category": "smartphones",
-        "thumbnail": "https://i.dummyjson.com/data/products/4/thumbnail.jpg"
-      },
-      {
-        "id": 5,
-        "title": "Huawei P30",
-        "description": "Huawei’s re-badged P30 Pro New Edition was officially unveiled yesterday in Germany and now the device has made its way to the UK.",
-        "price": 499,
-        "discountPercentage": 10.58,
-        "rating": 4.09,
-        "stock": 32,
-        "brand": "Huawei",
-        "category": "smartphones",
-        "thumbnail": "https://i.dummyjson.com/data/products/5/thumbnail.jpg"
-      }
-    ]
-  }
+const container = showContainer(dataUser)
+const modalContainer = document.querySelector('#container')
+modalContainer.innerHTML = container
+
+const modalCharts = document.querySelector('#cards')
+let cards = ``
+for (let i = 0; i < dataUser.charts.length; i++) {
+  cards += showCards(dataUser, i)
+}
+modalCharts.innerHTML = cards
